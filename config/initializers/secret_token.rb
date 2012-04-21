ab2f3732e36f515dc6418c7c3a7442d123e4dac8
Josh Baxter <joshbaxter13@googlemail.com>
2012-04-21 19:34:19 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BantbusApp::Application.config.secret_token = '3cde48247b9e0481ce02fc6009e6205fbd7e658d70c6027b6a13a8014e50057c5478f3b08e2295234c9c64453d155c7e42ed6eee4b06c87f1df3b601c8a3409c'
